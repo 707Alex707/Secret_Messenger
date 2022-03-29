ee7e9efcf017190c10c04b4d0fb3fe823c9637ed
Alexandre <alexandre707@live.com>
2022-03-29 17:52:31 -0400

    enable ssl

--- server/index.js
+++ server/index.js
@@ -1,27 +1,60 @@
+const fs = require('fs')
 const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-const { addUser, getUser, deleteUser, getUsers, getUsersInRoomExclusive } = require('./users')
+
+//SSL Configuration
+let sslConfig = {}
+sslConfig.publicKeyPath = "./server/certs/public.crt"
+sslConfig.privateKeyPath = "./server/certs/private.key"
+if (fs.existsSync(sslConfig.publicKeyPath) && fs.existsSync(sslConfig.privateKeyPath)){
+    sslConfig.publicKey = fs.readFileSync(sslConfig.publicKeyPath)
+    sslConfig.privateKey = fs.readFileSync(sslConfig.privateKeyPath)
+} else {
+    console.log("Public and/or private keys not found")
+    process.exit(1)
+}
+sslConfig.options = {
+    key: sslConfig.privateKey,
+    cert: sslConfig.publicKey,
+}
+
+//Redirect http to https
+const redirectApp = express()
+redirectApp.get("*", function(req, res, next) {
+    res.redirect("https://" + req.headers.host + req.path);
+});
+
+//Create http server
+let http = require('http').createServer(redirectApp)
+http.listen(80, function () {
+    console.log('Server listening on port 80')
+})
+
+//Create main app
+const app = express();
 
 //Serve home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(  __dirname + '/pages/index.html');
 });
 
 //Serve chat page
 app.get('/chat', (req, res) => {
-    res.sendFile(__dirname + '/chat.html');
+    res.sendFile(__dirname + '/pages/chat.html');
 });
 
 //Serve files in assets folders
-app.use('/assets', express.static('assets'));
+app.use('/public', express.static('public'));
 
-server.listen(8080, () => {
-    console.log('listening on *:8080');
-});
+//Create https server
+let httpsServer = require('https').createServer(sslConfig.options, app)
+httpsServer.listen(8443, function () {
+    console.log('Server listening on port 8443')
+})
+
+//Socket IO user message handling
+const io = new Server(httpsServer);
+const { addUser, getUser, deleteUser, getUsers, getUsersInRoomExclusive } = require('./users')
 
 
 io.on('connection', (socket) => {
